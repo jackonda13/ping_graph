78719422cee05c0251162fea8b614c6f7637ffb5
jackonda13 <jackond@mail.ru>
2023-03-13 18:06:53 +0600

    some customization

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ logfile = 'log.log'
 state = True
 ip = '100.41.9.9'
 
+
 def log(text, filename):
-    with open(filename, 'a+') as logfile:
-        logfile.write(f'{text} \n')
+    with open(filename, 'a+') as log_file:
+        log_file.write(f'{text} \n')
 
 
 while state:
@@ -20,5 +21,3 @@ while state:
     else:
         log(f'{time_now.strftime("%d.%m.%y %H:%M:%S")}, None', logfile)
     sleep(1)
-
-
