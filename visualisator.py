6389c651bd3a8280964431ba57fe1c5a8613f1b1
jackonda13 <jackond@mail.ru>
2023-03-13 13:42:09 +0600

    added graphs

--- visualisator.py
+++ visualisator.py
@@ -11,8 +11,28 @@ def get_data(filename):
             result[ip] = result.setdefault(ip, []) + [time_ping]
         return result
 
+def y_graph(log):
+    y_ = []
+    for i in log['10.41.9.9']:
+        y_.append(i[1])
+    return y_
 
-y = np.arange(get_data())
 
+def x_graph(log):
+    x_ = []
+    for i in log['10.41.9.9']:
+        x_.append(i[0])
+    return x_
 
-print(get_data('log.log'))
+filename_ = 'log.log'
+# y = np.arange(y_graph(get_data(filename_)))
+# x = np.arange(x_graph(get_data(filename_)))
+
+y = y_graph(get_data(filename_))
+x = x_graph(get_data(filename_))
+
+plt.plot(x, y)
+plt.xticks(rotation=90)
+plt.show()
+
+print(x_graph(get_data('log.log')))
